e3eed7948cdb4893d37d5bba75ed577e10154ccb
Elena Bayramova <elshishkanova@yandex.ru>
2023-08-16 21:33:21 +0300

    change cardTest

--- src/test/java/ApplicationCardTest.java
+++ src/test/java/ApplicationCardTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.codeborne.selenide.SelenideElement;
@@ -8,20 +9,24 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class ApplicationCardTest {
-
-    @Test
-    void positiveTestApplicationCard(){
+    @BeforeEach
+    void openSite() {
         open("http://localhost:9999");
 
+    }
+
+    @Test
+    void positiveTestApplicationCard() {
+
         $("[data-test-id=name] input").setValue("Елена Байрамова");
-      $("[data-test-id=phone] input").setValue("+79777306807");
-       $("[data-test-id=agreement]").click();
+        $("[data-test-id=phone] input").setValue("+79777306807");
+        $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Продолжить")).click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
     @Test
-    void positiveTestApplicationCardWithDash(){
-        open("http://localhost:9999");
+    void positiveTestApplicationCardWithDash() {
 
         $("[data-test-id=name] input").setValue("Елена-Елена Байрамова");
         $("[data-test-id=phone] input").setValue("+79777306807");
